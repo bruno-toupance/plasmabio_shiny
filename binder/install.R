4393747690ac3b3f034418af1c3657a04d21700d
Pierre Poulain <pierre.poulain@cupnet.net>
2022-07-03 22:46:07 +0200

    Try install.R

---
+++ binder/install.R
@@ -0,0 +1,9 @@
+install.packages("tidyverse")
+install.packages("diagram")
+# Install Bioconductor package manager
+# https://bioconductor.org/install/
+install.packages("BiocManager")
+BiocManager::install()
+# Biostrings: Efficient manipulation of biological strings
+# https://bioconductor.org/packages/release/bioc/html/Biostrings.html
+BiocManager::install("Biostrings")
